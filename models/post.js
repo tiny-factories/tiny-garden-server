4c5bb343b740ddad1a671ed7703bc00ba2689549
Glitch (tiny-garden-server) <none>
2021-02-06 08:36:17 +0000

    🐓🍂 Updated with Glitch: Discoed Schema Changes

--- models/post.js
+++ models/post.js
@@ -1,23 +1,27 @@
 // Data Model for Books
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+// var { nanoid } = require("nanoid");
+// var ID = nanoid();
 
-
-const MemberPostSchema = new Schema({
-  // If incoming req has these verables they will be saved in thie strucutre int he database.
+const MemberPostSchema = new Schema(
+  {
+    // If incoming req has these verables they will be saved in thie strucutre int he database.
+    // _id: "nanoid(12)",
     type: { type: String },
     content: { type: String },
-
-});
+    creatorId: { type: String },
+    source: { type: String },
+  },
+  { timestamps: true }
+);
 
 // Export model
 module.exports = mongoose.model("posts", MemberPostSchema);
 
-
 // // Data Model for Books
 // const mongoose = require("mongoose");
 // const Schema = mongoose.Schema;
-
 
 // const MemberPostSchema = new Schema({
 //   // If incoming req has these verables they will be saved in thie strucutre int he database.
@@ -27,7 +31,7 @@ module.exports = mongoose.model("posts", MemberPostSchema);
 
 //     slackChannelName: { type: String, required: true },
 //     slackChannelId: { type: String, required: true },
-  
+
 //     createdAt: { type: Date, required: true },
 //     content: { type: String, required: true }
 // });
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,24 @@
 // Route handlers
 const express = require("express");
 const router = express.Router();
+const MongoClient = require("mongodb").MongoClient;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+var { nanoid } = require("nanoid");
+var ID = nanoid();
+
+
+
+// Establish a connection with the Mongo Database
+// Get the username, password, host, and databse from the .env file
+const url = ("mongodb+srv://"+
+                 process.env.USERNAME+
+                 ":"
+                 +process.env.PASSWORD+
+                 "@"
+                 +process.env.HOST+
+                 "/"
+                 +process.env.DATABASE);
 
 //import data models
 const Book = require("../models/book");
@@ -8,15 +26,80 @@ const Member = require("../models/post");
 
 //CREATE Member Post
 router.post("/", function(req, res) {
-  let member = new Member(req.body);
   
+  // look up key based on discord ID
+// db.users.find( { "discordChannelId": "Hopper" } )
+  
+
+  const lookUpById = req.body[1];
+  // console.log(lookUpById);
+
+// var MongoClient = require('mongodb').MongoClient;
+// var mongoDB = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("tinyprofiles");
+  //Find the first document in the customers collection:
+  dbo.collection("users").findOne({}, function(err, result) {
+    if (err) throw err;
+    // console.log(result._id);
+    
+    
+    db.close();
+    
+  const creatorId = result._id;
+
+
+  let member = new Member(req.body);
+  // console.log(req.body[1])
   //Mapping incoming Post to Database Schema Post Info
+  member._id =  nanoid(12);
+  member.creatorId = creatorId;
   member.type = req.body[0].name;
+  member.source = "discord";
   member.content = req.body[0].value;
-  console.log(req);
+  // console.log(member);
   member.save(); // Save to db
   res.status(201).send("Saved to TinyProfiles db");
 });
+  
+    
+    
+    
+    
+  });
+});
+
+
+  
+  // @START: Will fix the connetion path for db.findone{} // then pass the discord ID into he quesrry from the current hard doced number // then finish schemafrom discord posts
+
+  
+  
+  
+//   let member = new Member(req.body);
+//   console.log(req.body[1])
+//   //Mapping incoming Post to Database Schema Post Info
+//   member._id =  nanoid(12);
+//   member.creatorId = result._id
+//   // member.creatorIdDiscord = 
+//   member.type = req.body[0].name;
+//   member.source = "discord";
+//   member.content = req.body[0].value;
+//   // console.log(req);
+//   member.save(); // Save to db
+//   res.status(201).send("Saved to TinyProfiles db");
+// });
+
+
+// _id :"MhssRbvK2lgp"
+// type :"post"
+// creatorId:req.body[2]
+// content:
+// "Test: Local post from webite editor."
+// discordChannelId:"website"
+// createdAt:2021-02-04T11:01:30.369+00:00:2021-02-04T11:01:30.369+00:00
 
 // EXAMPLES BASED ON SAVE TO A BOOK LIST
 
